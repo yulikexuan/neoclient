947ade44c9c402d26b911e18ba56dd9c6639fe1c
yulikexuan <yulikexuan@gmail.com>
2018-05-04 10:04:08 -0400

    Init commit

---
+++ src/main/java/com/amdocs/neoclient/NeoclientApplication.java
@@ -0,0 +1,18 @@
+//: com.amdocs.neoclient.NeoclientApplication.java
+
+
+package com.amdocs.neoclient;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NeoclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoclientApplication.class, args);
+	}
+
+}///:~
---
+++ src/test/java/com/amdocs/neoclient/NeoclientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amdocs.neoclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NeoclientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
